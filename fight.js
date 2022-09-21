a98515e66b354d3064fc05830976d6283951b086
shakazulu9 <harvin0715@gmailc.com>
2022-09-21 10:11:18 +1000

    X&O display

--- fight.js
+++ fight.js
@@ -1,4 +1,5 @@
 const tiles = document.querySelectorAll('.cell')
+const reset = document.getElementById('reset')
 const player_1 = 'X';
 const player_2 ='O';
 let turn = player_1;
@@ -9,26 +10,45 @@ boardPosition.fill(null);
 
 
 
-const playAgain = document.getElementById('reset')
 
 tiles.forEach((tile) => tile.addEventListener("click", tileClick));
 
+
+function setHoverText() {
+
+    tiles.forEach((tile) => {
+        tile.classList.remove("x-hover");
+        tile.classList.remove("o-hover");
+      });
+
+    const hoverClass = `${turn.toLowerCase()}-hover`;
+  
+    tiles.forEach((tile) => {
+      if (tile.innerText === "") {
+        tile.classList.add(hoverClass);
+      }
+    });
+  }
+  
+  setHoverText();
+
 function tileClick(event) {
-    const tiles = event.target;
+    const tile = event.target;
     const tileNumber = tile.dataset.index;
-    if (tiles.innerText != " ") {
+    if (tile.innerText != "") {
       return;
     }
   
     if (turn === player_1) {
-      tiles.innerText = player_1;
+      tile.innerText = player_1;
       boardPosition[tileNumber - 1] = player_1;
       turn = player_2;
     } else {
-      tiles.innerText = player_2;
+      tile.innerText = player_2;
       boardPosition[tileNumber - 1] = player_2;
       turn = player_1;
     }
 
+    setHoverText()
 }
-tileClick()
+
