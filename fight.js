cd2392d8f16244d8d9f3bf96d30cb2ea23654035
shakazulu9 <harvin0715@gmailc.com>
2022-09-22 12:12:24 +1000

    Tic Tac Toe

--- fight.js
+++ fight.js
@@ -1,97 +1,89 @@
-const tiles = document.querySelectorAll('.cell')
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
 
-const player_1 = 'X';
-const player_2 ='O';
-let turn = player_1;
+startGame()
 
+restartButton.addEventListener('click', startGame)
 
-const boardPosition = Array(tiles.length);
-boardPosition.fill(null); 
-
-const winnings = [
-    //rows
-    {combo: [1, 2, 3]},
-    {combo: [4, 5, 6]},
-    {combo: [7, 8, 9]},
-    //columns
-    {combo: [1, 4, 7]},
-    {combo: [2, 5, 8]},
-    {combo: [3, 6, 9]},
-    //diagonals
-    {combo: [1, 5, 9]},
-    {combo: [3, 5, 7]},
-  ];
-
-
-tiles.forEach((tile) => tile.addEventListener("click", tileClick));
-
-
-
-function setHoverText() {
-    const hoverClass = `${turn.toLowerCase()}-hover`;
-
-    tiles.forEach((tile) => {
-        tile.classList.remove("x-hover");
-        tile.classList.remove("o-hover");
-    });
-
-  
-    tiles.forEach((tile) => {
-      if (tile.innerText === "") {
-        tile.classList.add(hoverClass);
-      }
-    });
-  }
-  
-  setHoverText();
-
-function tileClick(event) {
-    const tile = event.target;
-    const tileNumber = tile.dataset.index;
-    if (tile.innerText != "") {
-      return;
-    }
-  
-    if (turn === player_1) {
-      tile.innerText = player_1;
-      boardPosition[tileNumber - 1] = player_1;
-      turn = player_2;
-    } else {
-      tile.innerText = player_2;
-      boardPosition[tileNumber - 1] = player_2;
-      turn = player_1;
-    }
-
-    setHoverText()
-    winner()
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
 }
 
-function winner() {
-    
-    for (const winningComb of winnings) {
-
-      const combo = winningComb.combo;
-    
-      const tileValue1 = boardPosition[combo[0] - 1];
-      const tileValue2 = boardPosition[combo[1] - 1];
-      const tileValue3 = boardPosition[combo[2] - 1];
-  
-      if (
-        tileValue1 != null &&
-        tileValue1 === tileValue2 &&
-        tileValue1 === tileValue3
-      ) 
-        return;
-        
-    }
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
 }
 
-const reset = document.getElementById('reset')
-reset.addEventListener('click', restart)
-
-function restart() {
-    boardPosition.fill(null);
-    tiles.forEach((tile) => (tile.innerText = ""));
-    turn = player_1;
-    setHoverText();
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
   }
+  winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
